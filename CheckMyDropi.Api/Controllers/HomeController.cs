dc334ae9134e09d0294156bde92e7f689c024f80
Lukasz W <lukasz_sabi@hotmail.com>
2020-03-27 23:27:18 +0100

    Initial

---
+++ CheckMyDropi.Api/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CheckMyDropi.Api.Models;
+using System.Net;
+using System.IO;
+using CheckMyDropi.Api.Services;
+
+namespace CheckMyDropi.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var webRequest = WebRequest.Create(@"https://raw.githubusercontent.com/littl3field/DodgyDomainsBot/master/COVID-Dodgy-Domains.txt");
+
+            using (var response = webRequest.GetResponse())
+            using (var content = response.GetResponseStream())
+            using (var reader = new StreamReader(content))
+            {
+                var strContent = reader.ReadToEnd();
+            }
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CheckMyDropi.Api/Services/SchedulerService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CheckMyDropi.Api.Services
+{
+    public class SchedulerService
+    {
+        private static SchedulerService _instance;
+        private List<Timer> timers = new List<Timer>();
+
+        private SchedulerService()
+        {
+
+        }
+
+        public static SchedulerService Instance => _instance ?? (_instance = new SchedulerService());
+
+
+        public static void IntervalInSeconds(int hour, int sec, double interval, Action task)
+        {
+            interval = interval / 3600;
+            Instance.ScheduleTask(hour, sec, interval, task);
+        }
+        public static void IntervalInMinutes(int hour, int min, double interval, Action task)
+        {
+            interval = interval / 60;
+            Instance.ScheduleTask(hour, min, interval, task);
+        }
+        public static void IntervalInHours(int hour, int min, double interval, Action task)
+        {
+            Instance.ScheduleTask(hour, min, interval, task);
+        }
+        public static void IntervalInDays(int hour, int min, double interval, Action task)
+        {
+            interval = interval * 24;
+            Instance.ScheduleTask(hour, min, interval, task);
+        }
+
+
+        private void ScheduleTask(int hour, int min, double intervalInHour, Action task)
+        {
+            DateTime now = DateTime.Now;
+            DateTime firstRun = new DateTime(now.Year, now.Month, now.Day, hour, min, 0, 0);
+            if (now > firstRun)
+            {
+                firstRun = firstRun.AddDays(1);
+            }
+            TimeSpan timeToGo = firstRun - now;
+            if (timeToGo <= TimeSpan.Zero)
+            {
+                timeToGo = TimeSpan.Zero;
+            }
+            var timer = new Timer(x =>
+            {
+                task.Invoke();
+            }, null, timeToGo, TimeSpan.FromHours(intervalInHour));
+            timers.Add(timer);
+        }
+    }
+}
