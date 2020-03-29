2ff5539f8b554c68949524ce03e2b412af067c6d
andreamarinosa <andreamarinosalopez@gmail.com>
2020-03-29 03:44:35 +0200

    prevent default added and new about text

--- CheckMyDropi.Api/wwwroot/js/site.js
+++ CheckMyDropi.Api/wwwroot/js/site.js
@@ -1,10 +1,13 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+import { Zoom } from "swiper/js/swiper.esm";
+
 // Write your JavaScript code.
 
 $(document).ready(function () {
-    $("#searchButton").click(function () {
+    $("#searchButton").click(function (event) {
+        event.preventDefault();
         const url = encodeURI($("#textUrlSearch").val());
         $.ajax({
             url: '/api/v1/url/'+url+'/check',
@@ -13,6 +16,9 @@ $(document).ready(function () {
                 $('#info').html('<p>An error has occurred</p>');
             },
             success: function (data) {
+                if (data) {
+
+                }
                 alert(data);
             }
         });
