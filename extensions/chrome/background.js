dadb807b14e3d0a99204ced73f759d95751e572c
Lukasz W <lukasz_sabi@hotmail.com>
2020-03-29 01:06:07 +0100

    Add Cherome extension

---
+++ extensions/chrome/background.js
@@ -0,0 +1,28 @@
+'use strict';
+  chrome.tabs.onUpdated.addListener(
+    function(tabId, changeInfo, tab) {
+      if (changeInfo.url) {
+          if(changeInfo.url.indexOf("http") == -1 ){
+              return
+          }
+        var url = changeInfo.url.split("/")[2];
+         url = encodeURI(url);
+        $.ajax({
+            url: 'https://localhost:32808/api/v1/url/'+url+'/check',
+            type: 'GET',
+            crossDomain : true,
+            error: function (data) {
+                console.log(data);
+                //alert(data);
+            },
+            success: function (data) {
+                console.log(data)
+                if(data.malicius){
+                    alert("This site has been flagged by DROPi as suspicious,\nplease proceed with caution as this could be a phishing link or fake news!");
+                }
+                
+            }
+        });
+      }
+    }
+  );
