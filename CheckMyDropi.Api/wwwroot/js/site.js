cae8ab82c19023ee1f43cb71d3e8e88a9e65843b
andreamarinosa <andreamarinosalopez@gmail.com>
2020-03-30 11:08:28 +0200

    check if its an empty url

--- CheckMyDropi.Api/wwwroot/js/site.js
+++ CheckMyDropi.Api/wwwroot/js/site.js
@@ -7,24 +7,28 @@ $(document).ready(function () {
 
     $("#formCheck").on("submit", function (event) {
         event.preventDefault();
-        var url = encodeURI($("#textUrlSearch").val());
-        if (url.indexOf("http") > -1) {
-            url = changeInfo.url.split("/")[2];
-        }
-        url = encodeURI(url);
-        $.ajax({
-            url: '/api/v1/url/' + url + '/check',
-            type: 'GET',
-            error: function () {
-                $('#info').html('<p>An error has occurred</p>');
-            },
-            success: function (data) {
-                if (data.malicius) {
-                    $('#info').html('<p class="danger">This site has been flagged by DROPi as suspicious,\nplease proceed with caution as this could be a phishing link or fake news!</p>');
-                } else {
-                    $('#info').html('<p class="good">This site is clean and good to go!</p>');
-                }
+        if ($("#textUrlSearch").val()) {
+            var url = encodeURI($("#textUrlSearch").val());
+            if (url.indexOf("http") > -1) {
+                url = changeInfo.url.split("/")[2];
             }
-        });
+            url = encodeURI(url);
+            $.ajax({
+                url: '/api/v1/url/' + url + '/check',
+                type: 'GET',
+                error: function () {
+                    $('#info').html('<p>An error has occurred</p>');
+                },
+                success: function (data) {
+                    if (data.malicius) {
+                        $('#info').html('<p class="danger">This site has been flagged by DROPi as suspicious,\nplease proceed with caution as this could be a phishing link or fake news!</p>');
+                    } else {
+                        $('#info').html('<p class="good">This site is clean and good to go!</p>');
+                    }
+                }
+            });
+        } else {
+            $('#info').html('<p class="empty-url">There is not an url!</p>');
+        }
     });
 });
